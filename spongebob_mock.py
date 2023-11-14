eb1ca633f64e38246a5c51d47fcd1e44f3dade6f
darkcoffee8 <142404661+darkcoffee8@users.noreply.github.com>
2023-11-13 22:25:42 -0400

    Add files via upload

---
+++ spongebob_mock.py
@@ -0,0 +1,12 @@
+def spongemock():
+    inp = input("Enter text here, and watch this software mock you\n>")
+    mock=[]
+    for i in inp:
+        if i.index(i.index() % 2 == 0):
+            i.upper()
+        else:
+            i.lower()
+    mock.append(str("".join(i)))
+    print(mock)
+
+spongemock()

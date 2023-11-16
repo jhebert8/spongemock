17650b19cc9b0e2ad4784518885cad88ab354a21
darkcoffee8 <142404661+darkcoffee8@users.noreply.github.com>
2023-11-16 13:05:52 -0400

    Update spongebob_mock.py
    Another try.

--- spongebob_mock.py
+++ spongebob_mock.py
@@ -1,13 +1,11 @@
-def spongemock():
-    inp = input("Enter text here, and watch this software mock you\n>")
-    mock=[]
-    for i in inp:
-        if i.index(i.index() % 2 == 0):
-            i.upper()
-        else:
-            i.lower()
-    mock.append(str("".join(i)))
-    print(mock)
+mock = input("type something here and watch this piece of software mock you:\n>")
 
-spongemock()
+for i in mock:
+    if i.index(i) % 2 == 0:
+        print(i.upper())
+    elif i.index(i) % 2 != 0:
+        print(i.lower())
+    print("".join(i))
+
 # Still have a lot to figure out about this one. Gotta make sure we fix the index part, but unsure how to proceed.
+# Don't forget to make a function out of it so it can be used by other memers
